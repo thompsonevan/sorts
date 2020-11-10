ff30c56a5066c24d0c6536c27f5634e59ccf5a6d
thompsonevan <evan.thompson515@gmail.com>
2020-11-10 13:56:58 -0500

    prob just selection

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-trying to understand sorting algorithims like brute, merge, and bubble.
+trying to understand sorting algorithims like selection(brute), merge, and bubble.
--- mine.py
+++ mine.py
@@ -1,14 +1,29 @@
 # Trying my own way to sort if it looks liek anyother alogirthim then so be it.
-import sys
-
+# I think this is just bad selection sort
 def sort(list):
-	lastNum = None
-	lowestNum = sys.maxint
 	highestNum = 0
+	newList = []
 
-	for x in list:
+	for y in list:
+		if y > highestNum:
+			highestNum = y		
 
+	while len(newList) == 0 or newList[-1] != highestNum:
+		lowestNum = 9999999999
 
-list = [20, 50, 23, 84, 1, 9]
+		for x in list:
+			if x < lowestNum:
+				lowestNum = x
 
-sort(list)
+		print(newList)
+
+		list.remove(lowestNum)
+		newList.append(lowestNum)
+		
+	return newList
+
+list = [20, 40, 30, 20, 100, 90, 80, 60, 70, 20, 50, 23, 1, 84, 1, 23, 9]
+
+thing = sort(list)
+
+print(thing)
