b14b25570f8636f4272a1dc5da17cf87ed58c8c9
thompsonevan <evan.thompson515@gmail.com>
2020-11-10 09:47:36 -0500

    stuff

---
+++ mine.py
@@ -0,0 +1,14 @@
+# Trying my own way to sort if it looks liek anyother alogirthim then so be it.
+import sys
+
+def sort(list):
+	lastNum = None
+	lowestNum = sys.maxint
+	highestNum = 0
+
+	for x in list:
+
+
+list = [20, 50, 23, 84, 1, 9]
+
+sort(list)
